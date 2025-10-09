ac53f046df89dd7bca6dc85f771674423cd79964
דניאל חנוכייב <Hanudani@gmail.com>
2025-10-09 17:38:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "lionspace",
+  "appId": "1:707897822334:web:a15bfb2e2680f20a055c2f",
+  "apiKey": "AIzaSyAbD34V_iSBPmrB7qgbInigGFElT7CYoKQ",
+  "authDomain": "lionspace.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "707897822334"
+};
